2f9bacc76e20df507e41894a2528171f5be36e36
Karnan10 <karnan.yogarajah@ontariotechu.net>
2021-09-27 14:13:31 -0400

    Lab 01 completed code

--- lab01.py
+++ lab01.py
@@ -1,12 +1,22 @@
 def main():
     cost_per_item = 19.99
     quantity = 5 
+    subtotal_cost = 0
+    tax = 0.13
+    total_cost = 0
 
     # YOUR CODE TO CALCULATE THE TOTALS GOES HERE    
-
-    print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
+    subtotal_cost = cost_per_item *quantity
+    tax = subtotal_cost * 0.13
+    total_cost = subtotal_cost + tax
 
     # YOUR CODE TO PRINT THE TOTALS GOES HERE
+    print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
+    print(f'quantity = {quantity}')
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}')
+    print(f'tax = ${tax:0.2f}')
+    print(f'total_cost = ${total_cost:0.2f}')
 
+    
 if __name__ == "__main__":
     main()
